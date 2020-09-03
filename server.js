49bf8867c13e6e76d82d90a3a62b3a91ba02457c
slawoe <info@slawomirernst.de>
2020-09-03 12:25:24 +0200

    Serve client

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000; // Heroku gibt Port vor, ansonsten nimmt 3000
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const port = process.env.PORT || 3000;
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
